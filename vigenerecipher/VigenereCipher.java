ba425ce47a289d9d5df8b1f8bc9b55d19dac2072
Goddamnnoob <gowthamreddy547@gmail.com>
2021-05-11 11:17:03 +0530

    Vigenere Cipher: encryption with default key

--- vigenerecipher/VigenereCipher.java
+++ vigenerecipher/VigenereCipher.java
@@ -3,15 +3,28 @@ import java.util.Scanner;
 
 public class VigenereCipher {
 
-    public void encrypt(){
+    public void encrypt(){ 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter PlainText:");
-        String pt = input.nextLine();
-        System.out.println("Enter key:");
-        String key = input.nextLine();
-        createSquareTable();
+        char[] pt = input.nextLine().toCharArray();
+        char[] ct = new char[pt.length];
+        char[] key = new char[pt.length];
+        System.out.println("Key: ");
+        for(int i=0;i<pt.length;i++){
+            key[i] = (char) ('a' + i );
+            System.out.print(key[i]);
+        }
+        System.out.println(" ");
+        char[][] sqtbl = createSquareTable();
+        for(int i=0;i<pt.length;i++){
+            int r = pt[i] - 'a';
+            int c = key[i] - 'a';
+            ct[i] = sqtbl[r +1 ][c + 1];
+            System.out.print(ct[i]);
+        }
+        input.close();
     }
-    private void createSquareTable(){
+    private char[][] createSquareTable(){
         char[][] sqtbl = new char[27][27];
         for(int i=1;i<=26;i++){
             sqtbl[0][i] = (char) (( i - 1 ) + 'a');
@@ -20,5 +33,6 @@ public class VigenereCipher {
                 sqtbl[i][j] = (char) ('a' + ( ((sqtbl[i][0] - 'a') + (j - 1)) % 26 ));
             }
         } 
+        return sqtbl;
     }
 }
