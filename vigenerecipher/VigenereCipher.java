1927117ac923648135e49ad6fc95dcfd85474cef
Goddamnnoob <gowthamreddy547@gmail.com>
2021-05-11 11:49:45 +0530

    Vigenere Cipher: implemented decryption

--- vigenerecipher/VigenereCipher.java
+++ vigenerecipher/VigenereCipher.java
@@ -2,6 +2,7 @@ package vigenerecipher;
 import java.util.Scanner;
 
 public class VigenereCipher {
+    char[][] sqtbl = new char[27][27];
 
     public void encrypt(){ 
         Scanner input = new Scanner(System.in);
@@ -15,7 +16,7 @@ public class VigenereCipher {
             System.out.print(key[i]);
         }
         System.out.println(" ");
-        char[][] sqtbl = createSquareTable();
+        createSquareTable();
         for(int i=0;i<pt.length;i++){
             int r = pt[i] - 'a';
             int c = key[i] - 'a';
@@ -23,9 +24,10 @@ public class VigenereCipher {
             System.out.print(ct[i]);
         }
         input.close();
+        System.out.println("");
+        decrypt(ct, key);
     }
-    private char[][] createSquareTable(){
-        char[][] sqtbl = new char[27][27];
+    private void createSquareTable(){
         for(int i=1;i<=26;i++){
             sqtbl[0][i] = (char) (( i - 1 ) + 'a');
             sqtbl[i][0] = (char) (( i - 1 ) + 'a');
@@ -33,6 +35,18 @@ public class VigenereCipher {
                 sqtbl[i][j] = (char) ('a' + ( ((sqtbl[i][0] - 'a') + (j - 1)) % 26 ));
             }
         } 
-        return sqtbl;
+    }
+    public void decrypt(char[] ct,char[] key){
+        char[] pt = new char[ct.length];
+        for(int i=0;i<ct.length;i++){
+            int r = key[i] - 'a';
+            for(int c=1;c<27;c++){
+                if(sqtbl[r+1][c]==ct[i]){
+                    pt[i] = sqtbl[0][c];
+                }
+            }
+            System.out.print(pt[i]);
+        }
+        System.out.println("");
     }
 }
