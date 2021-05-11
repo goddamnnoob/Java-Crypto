e9b0a729860a7b5465f956ecd04a5cff188fac7b
Goddamnnoob <gowthamreddy547@gmail.com>
2021-05-11 12:37:43 +0530

    Vernam Cipher

--- main.java
+++ main.java
@@ -1,13 +1,15 @@
 //import monoalphabeticcipher.MonoAlphabeticCipher;
 //import caesarcipher.*;
 //import affinecipher.*;
-import vigenerecipher.*;
+//import vigenerecipher.*;
+import vernamcipher.*;
 class Main {
     public static void main(String[] args) {
         //MonoAlphabeticCipher m = new MonoAlphabeticCipher();
         //CaesarCipher c = new CaesarCipher();
         //AffineCipher a = new AffineCipher();
-        VigenereCipher v = new VigenereCipher();
+        //VigenereCipher v = new VigenereCipher();
+        VernamCipher v = new VernamCipher();
         v.encrypt();
     }
 }
---
+++ vernamcipher/VernamCipher.java
@@ -0,0 +1,53 @@
+package vernamcipher;
+import java.util.Scanner;
+
+public class VernamCipher {
+    char[][] sqtbl = new char[27][27];
+
+    public void encrypt(){ 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter PlainText:");
+        char[] pt = input.nextLine().toCharArray();
+        char[] ct = new char[pt.length];
+        System.out.println("Enter Key: ");
+        char[] key = input.nextLine().toCharArray(); 
+        if(key.length != pt.length){
+            System.out.println("Key and Plain Text should be of equal size. ");
+        }else {
+            System.out.println(" ");
+            createSquareTable();
+            for(int i=0;i<pt.length;i++){
+                int r = pt[i] - 'a';
+                int c = key[i] - 'a';
+                ct[i] = sqtbl[r +1 ][c + 1];
+                System.out.print(ct[i]);
+            }
+            System.out.println("");
+            decrypt(ct, key);            
+        }
+
+        input.close();
+    }
+    private void createSquareTable(){
+        for(int i=1;i<=26;i++){
+            sqtbl[0][i] = (char) (( i - 1 ) + 'a');
+            sqtbl[i][0] = (char) (( i - 1 ) + 'a');
+            for(int j=1;j<=26;j++){
+                sqtbl[i][j] = (char) ('a' + ( ((sqtbl[i][0] - 'a') + (j - 1)) % 26 ));
+            }
+        } 
+    }
+    public void decrypt(char[] ct,char[] key){
+        char[] pt = new char[ct.length];
+        for(int i=0;i<ct.length;i++){
+            int r = key[i] - 'a';
+            for(int c=1;c<27;c++){
+                if(sqtbl[r+1][c]==ct[i]){
+                    pt[i] = sqtbl[0][c];
+                }
+            }
+            System.out.print(pt[i]);
+        }
+        System.out.println("");
+    }
+}
